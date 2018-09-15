b2c5c2954839596bdbe409201354e95743128957
melqui <melqui@localhost>
2018-09-15 12:14:51 -0500

    Interfaz de empresas

--- gcs/database/seeds/EnterprisesSeeder.php
+++ gcs/database/seeds/EnterprisesSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
+//use Faker\Factory as Faker;
 
 class EnterprisesSeeder extends Seeder {
 
@@ -11,19 +11,7 @@ class EnterprisesSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        $faker = Faker::create();
-        for ($i = 0; $i < 10; $i++) {
-            \DB::table('enterprises')->insert(array(
-                'name' => 'Empresa ' . $faker->number,
-                'street' => $faker->randomElement(['c. 10', 'c. 35', 'c. 60', 'c. 82']),
-                'borough' => $faker->randomElement(['Chicxulub', 'Progreso', 'San Ignacio']),
-                'city' => $faker->randomElement(['Merida', 'Progreso', 'San Valladolid']),
-                'state' => $faker->randomElement(['Yucatán', 'CA', 'San Ignacio']),
-                'cp' => $faker->randomElement(['97000', '97320', '97000']),
-                'timestamps' => date('Y-m-d H:m:s'),
 
-            ));
-        }
     }
 
 }
